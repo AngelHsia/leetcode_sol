302be6ce74822f9a3345d7a0614201670005d877
AngelHsia <angelhsia90@gmail.com>
2023-11-25 12:45:17 +0800

    231125 daily question

---
+++ 231125_Sum of Absolute Differences in a Sorted Array.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        Node* og_current_node = head;
+        Node* new_nodes(0);
+        while(og_current_node != NULL){
+            
+            return 0;
+        }
+    }
+};
+
+int main(){
+    return 0;
+}
